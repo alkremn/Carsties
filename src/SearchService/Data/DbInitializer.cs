e7fed26aa5090caa42e2114cc873bf87c0af0bdd
Alexey Kremnev <alkremn@gmail.com>
2024-01-21 21:25:49 -0700

    End of Section 3

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings
+            .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        if (count > 0)
+            return;
+
+        System.Console.WriteLine("getting data from auction service");
+
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvsHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        await DB.SaveAsync(items);
+    }
+}
